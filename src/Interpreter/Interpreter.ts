a5dd0202e978cd7fbec36512300cdc625aec9be4
Koen Vendrik <k.vendrik@gmail.com>
2020-12-05 17:25:34 -0800

    cleans up interpreter

--- src/Interpreter/Interpreter.ts
+++ src/Interpreter/Interpreter.ts
@@ -10,110 +10,22 @@ import {
   Operator,
   BooleanValue,
 } from '../parser';
+import builtIns from './builtIns';
 
 export enum ObfuscatedValue {
   Function = '[Function]',
 }
 
+type StoredExpression = Token | FunctionExpression;
+
+export type Context = Map<string, StoredExpression>;
+
 enum Keyword {
   Break = 'break',
 }
 
-type StoredExpression = Token | FunctionExpression;
-
-const BUILT_INS: {
-  [name: string]: (
-    parameters: Event[],
-    context: Map<string, StoredExpression>,
-  ) => Token | void;
-} = {
-  log: (parameters, context) => {
-    const tokens = parameters.map((parameter) => {
-      const interpreter = new Interpreter(context);
-      return interpreter.evaluate([parameter]);
-    });
-    console.log(...tokens.map((token) => (token ? token.value : null)));
-  },
-  concat(parameters, context) {
-    const tokens = parameters.map((parameter) => {
-      const interpreter = new Interpreter(context);
-      return interpreter.evaluate([parameter]);
-    });
-
-    if (
-      tokens.some(
-        (token) =>
-          token == null ||
-          (token.type !== TokenType.String && token.type !== TokenType.Number),
-      )
-    ) {
-      throw new Error('concat() can only be used with strings');
-    }
-
-    const resolvedTokens = tokens as Token[];
-    const resultString = resolvedTokens.reduce(
-      (current, {value}) => (current !== '' ? `${current} ${value}` : value),
-      '',
-    );
-    return {type: TokenType.String, value: resultString};
-  },
-  if([testEvent, handlerEvent], context) {
-    const interpreter = new Interpreter(context);
-    const testResult = interpreter.evaluate([testEvent]);
-
-    if (
-      testResult == null ||
-      testResult.type !== TokenType.Boolean ||
-      testResult.value !== BooleanValue.True
-    ) {
-      return;
-    }
-
-    if (handlerEvent.type !== EventType.FunctionExpression) {
-      const handlerResult = interpreter.evaluate([handlerEvent]);
-      return handlerResult;
-    }
-
-    if (handlerEvent.body == null) {
-      throw new Error(
-        'Tried to resolve if() using function call but function has no body.',
-      );
-    }
-
-    return interpreter.evaluate(handlerEvent.body);
-  },
-  while([testEvent, handlerEvent], context) {
-    const interpreter = new Interpreter(context);
-    let testResult = interpreter.evaluate([testEvent]);
-
-    if (
-      testResult == null ||
-      testResult.type !== TokenType.Boolean ||
-      testResult.value !== BooleanValue.True
-    ) {
-      return;
-    }
-
-    if (handlerEvent.type !== EventType.FunctionExpression) {
-      throw new Error('while() handler is not a function.');
-    }
-
-    if (handlerEvent.body == null) {
-      throw new Error('while() handler has no body.');
-    }
-
-    interpreter.evaluate(handlerEvent.body);
-    this.while([testEvent, handlerEvent], context);
-  },
-};
-
 export default class Interpreter {
-  constructor(
-    private context: Map<string, StoredExpression> = new Map<
-      string,
-      StoredExpression
-    >(),
-  ) {}
+  constructor(private context: Context = new Map<string, StoredExpression>()) {}
 
   evaluate(events: Event[]): Token | void {
     for (const [index, event] of events.entries()) {
@@ -261,10 +173,10 @@ export default class Interpreter {
   }
 
   private handleFunctionCall({symbol, parameters}: FunctionCall): Token | void {
-    const builtIn = BUILT_INS[symbol];
+    const builtIn = builtIns[symbol];
 
     if (builtIn) {
-      return builtIn.call(BUILT_INS, parameters, this.context);
+      return builtIn.call(builtIns, parameters, this.context);
     }
 
     const functionExpression = this.context.get(symbol);
---
+++ src/Interpreter/builtIns.ts
@@ -0,0 +1,90 @@
+import {Event, EventType, Token, TokenType, BooleanValue} from '../parser';
+import Interpreter, {Context} from './Interpreter';
+
+const buildIns: {
+  [name: string]: (parameters: Event[], context: Context) => Token | void;
+} = {
+  log: (parameters, context) => {
+    const tokens = parameters.map((parameter) => {
+      const interpreter = new Interpreter(context);
+      return interpreter.evaluate([parameter]);
+    });
+    console.log(...tokens.map((token) => (token ? token.value : null)));
+  },
+
+  concat(parameters, context) {
+    const tokens = parameters.map((parameter) => {
+      const interpreter = new Interpreter(context);
+      return interpreter.evaluate([parameter]);
+    });
+
+    if (
+      tokens.some(
+        (token) =>
+          token == null ||
+          (token.type !== TokenType.String && token.type !== TokenType.Number),
+      )
+    ) {
+      throw new Error('concat() can only be used with strings');
+    }
+
+    const resolvedTokens = tokens as Token[];
+    const resultString = resolvedTokens.reduce(
+      (current, {value}) => (current !== '' ? `${current} ${value}` : value),
+      '',
+    );
+    return {type: TokenType.String, value: resultString};
+  },
+
+  if([testEvent, handlerEvent], context) {
+    const interpreter = new Interpreter(context);
+    const testResult = interpreter.evaluate([testEvent]);
+
+    if (
+      testResult == null ||
+      testResult.type !== TokenType.Boolean ||
+      testResult.value !== BooleanValue.True
+    ) {
+      return;
+    }
+
+    if (handlerEvent.type !== EventType.FunctionExpression) {
+      const handlerResult = interpreter.evaluate([handlerEvent]);
+      return handlerResult;
+    }
+
+    if (handlerEvent.body == null) {
+      throw new Error(
+        'Tried to resolve if() using function call but function has no body.',
+      );
+    }
+
+    return interpreter.evaluate(handlerEvent.body);
+  },
+
+  while([testEvent, handlerEvent], context) {
+    const interpreter = new Interpreter(context);
+    let testResult = interpreter.evaluate([testEvent]);
+
+    if (
+      testResult == null ||
+      testResult.type !== TokenType.Boolean ||
+      testResult.value !== BooleanValue.True
+    ) {
+      return;
+    }
+
+    if (handlerEvent.type !== EventType.FunctionExpression) {
+      throw new Error('while() handler is not a function.');
+    }
+
+    if (handlerEvent.body == null) {
+      throw new Error('while() handler has no body.');
+    }
+
+    interpreter.evaluate(handlerEvent.body);
+    this.while([testEvent, handlerEvent], context);
+  },
+};
+
+export default buildIns;
